3fce8f1433825e5273c6e7458ae47864555cd308
timchu <timchu>
2016-06-12 22:01:33 -0400

    Added pause feature. p is pause. r is resume.

--- slimeMain.js
+++ slimeMain.js
@@ -5,12 +5,14 @@ var ball;
 var floor = 400;
 var leftWallX = 0;
 var rightWallX = 1000;
+var net;
 var netX = (leftWallX + rightWallX)/2;
 var netWidth = 4;
 var netHeight = 35;
 
-// This is used in ball.js. This should all be cleaned up.
-var acceleration = 0.5;
+// var overScreen;
+// // This is used in ball.js. This should all be cleaned up.
+// var acceleration = 0.5;
 
 function collideWalls(ball) {
   if (ball.y >=  floor - ball.radius) {
@@ -42,26 +44,52 @@ function collideNet(ball) {
   }
 }
 var initSlimeX = 180;
+function initGame() {
+  redSlime = stage.addChild(makeCircleSlime("Red", 'up', 'down', 'left', 'right', rightWallX - initSlimeX));
+  blueSlime = stage.addChild(makeCircleSlime("DeepSkyBlue", 'w','s','a','d', initSlimeX));
+  net = new createjs.Shape();
+  net.graphics.beginFill("Black").drawRect(netX - netWidth/2, floor - netHeight, netWidth, netHeight);
+  stage.addChild(net);
+
+  // Do not add overscreen yet.
+  overScreen = new createjs.Shape();
+  overScreen.graphics.beginFill("Green").drawRect(0, 0, 1000, 1000);
+
+  ball = stage.addChild(makeBall("Black", initSlimeX, 200));
+}
+function resetGame() {
+  stage.removeChild(ball);
+  //stage.removeChild(overScreen);
+  stage.removeChild(redSlime);
+  stage.removeChild(blueSlime);
+}
 function init() {
-    stage = new createjs.Stage("demoCanvas");
-    redSlime = stage.addChild(makeCircleSlime("Red", 'up', 'down', 'left', 'right', rightWallX - initSlimeX));
-    blueSlime = stage.addChild(makeCircleSlime("DeepSkyBlue", 'w','s','a','d', initSlimeX));
-    var net = new createjs.Shape();
-    net.graphics.beginFill("Black").drawRect(netX - netWidth/2, floor - netHeight, netWidth, netHeight);
-    stage.addChild(net);
+  stage = new createjs.Stage("demoCanvas");
+  initGame();
+  createjs.Ticker.setFPS(60);
+  createjs.Ticker.addEventListener("tick", stage);
+  createjs.Ticker.addEventListener("tick", tick);
 
-    ball = stage.addChild(makeBall("Black", initSlimeX, 100));
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", stage);
-    createjs.Ticker.addEventListener("tick", tick);
-
-    function tick(event) {
+  // var over = false;
+  var paused = false;
+  function tick(event) {
+    if (!paused) {
       collideWalls(ball);
       collideNet(ball);
       updateSlime(floor, netX + netWidth, rightWallX, redSlime);
       updateSlime(floor, leftWallX, netX, blueSlime);
       updateBall(redSlime, blueSlime, ball);
     }
+    if (key.isPressed('p')){
+      //stage.addChild(overScreen);
+      paused = true;
+    }
+    if (key.isPressed('r')){
+      paused = false;
+      //resetGame();
+      //initGame(stage);
+    }
+  }
 }
 
 // Suppressing up down left right.
