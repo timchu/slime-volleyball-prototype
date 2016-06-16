0f1d7fa99a39efbacdc0f625a36147b2c5137ccd
timchu <timchu>
2016-06-16 00:11:57 -0400

    Made slime into an actual game.

--- slimeMain.js
+++ slimeMain.js
@@ -10,17 +10,54 @@ var netX = (leftWallX + rightWallX)/2;
 var netWidth = 4;
 var netHeight = 35;
 
+var scoreContainer;
+var playerOneScore;
+var playerTwoScore;
+//  Enum used to keep track of game state.
+var state;
+
+var stateEnum = {
+  IN_GAME: 0,
+  AFTER_POINT: 1,
+  PAUSED_GAME: 2,
+  END_GAME: 3
+}
+
+
+
 // var overScreen;
 // // This is used in ball.js. This should all be cleaned up.
 // var acceleration = 0.5;
 
 function collideWalls(ball) {
-  if (ball.y >=  floor - ball.radius) {
-    ball.y = floor - ball.radius
-    ball.ySpeed = - ball.ySpeed;
-  }
   if (ball.x <= leftWallX + ball.radius || ball.x >= rightWallX - ball.radius) { 
     ball.xSpeed = - ball.xSpeed;
+  }
+}
+
+// TODO: make this defeat the slime.
+function checkFloor(ball) {
+  if (ball.y >=  floor - ball.radius) {
+    if (ball.x  <= netX) {
+      playerOneScore -= 1;
+      playerTwoScore += 1;
+    }
+    else {
+      playerOneScore += 1;
+      playerTwoScore -= 1;
+      if (playerTwoScore == 0){
+        state = stateEnum.END_GAME;
+      }
+    }
+    if (playerOneScore == 0){
+      state = stateEnum.END_GAME;
+    }
+    else if (playerTwoScore == 0){
+      state = stateEnum.END_GAME;
+    }
+    else {
+      state = stateEnum.AFTER_POINT;
+    }
   }
 }
 
@@ -43,8 +80,13 @@ function collideNet(ball) {
     }
   }
 }
-var initSlimeX = 180;
+
+
+
 function initGame() {
+  var initSlimeX = 180;
+  state = stateEnum.IN_GAME;
+
   redSlime = stage.addChild(makeCircleSlime("Red", 'up', 'down', 'left', 'right', rightWallX - initSlimeX));
   blueSlime = stage.addChild(makeCircleSlime("DeepSkyBlue", 'w','s','a','d', initSlimeX));
   net = new createjs.Shape();
@@ -56,38 +98,77 @@ function initGame() {
   overScreen.graphics.beginFill("Green").drawRect(0, 0, 1000, 1000);
 
   ball = stage.addChild(makeBall("Black", initSlimeX, 200));
+
+  scoreContainer = new createjs.Container();
+  displayScore(playerOneScore, playerTwoScore);
+
 }
-function resetGame() {
+
+function clearGame() {
   stage.removeChild(ball);
-  //stage.removeChild(overScreen);
   stage.removeChild(redSlime);
   stage.removeChild(blueSlime);
+  stage.removeChild(scoreContainer);
 }
+
+function resetGameAfterPoint() {
+  clearGame();
+  initGame();
+}
+
+function setDefaultScore () {
+  playerOneScore = 5;
+  playerTwoScore = 5;
+}
+
+function resetGameFromBeginning() {
+  setDefaultScore();
+  clearGame();
+  initGame();
+}
+
+function displayScore(a, b){
+  for (var i = 0; i < a; ++i) {
+    // hardcoded arbitrary constants.
+    // makeBall should not be overloaded. This is lazy code.
+    scoreContainer.addChild(makeBall("DeepSkyBlue", 36 + 60*i, 60, 18));
+  }
+  for (var i = 0; i < b; ++i) {
+    scoreContainer.addChild(makeBall("Red", rightWallX - 36 - 60*i, 60, 18));
+  }
+  stage.addChild(scoreContainer);
+}
+
 function init() {
   stage = new createjs.Stage("demoCanvas");
+  setDefaultScore();
   initGame();
   createjs.Ticker.setFPS(60);
   createjs.Ticker.addEventListener("tick", stage);
   createjs.Ticker.addEventListener("tick", tick);
 
-  // var over = false;
-  var paused = false;
   function tick(event) {
-    if (!paused) {
+    if (state == stateEnum.IN_GAME) {
+      if (key.isPressed('p')){
+        state = stateEnum.PAUSED;
+        stage.removeChild(scoreContainer);
+      }
       collideWalls(ball);
       collideNet(ball);
       updateSlime(floor, netX + netWidth, rightWallX, redSlime);
       updateSlime(floor, leftWallX, netX, blueSlime);
       updateBall(redSlime, blueSlime, ball);
+      checkFloor(ball);
     }
-    if (key.isPressed('p')){
-      //stage.addChild(overScreen);
-      paused = true;
+    else if (state == stateEnum.AFTER_POINT){
+      if (key.getPressedKeyCodes() > 0) {
+        resetGameAfterPoint();
+      }
     }
-    if (key.isPressed('r')){
-      paused = false;
-      //resetGame();
-      //initGame(stage);
+    else if (state == stateEnum.PAUSED){
+      if (key.isPressed('r')) {
+        state = stateEnum.IN_GAME;
+      }
     }
   }
 }
